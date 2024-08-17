56951b341dd4a7677133a49dda5014f6f7bc8d74
Madswolf <mads@madswolf.dk>
2024-08-17 02:13:38 +0200

    less chatter

--- poll_bot.py
+++ poll_bot.py
@@ -59,7 +59,7 @@ async def log_reaction(message, reaction, user):
     emoji = reaction.emoji
     memeId = search_filename(message.attachments[0].filename)
     headers, data = prepare_vote_data(user, memeId, str(emoji)[0])
-    await post_vote(bot.get_channel(reaction.message.channel.id), headers, data, True)
+    await post_vote(bot.get_channel(reaction.message.channel.id), headers, data, False)
 
 def react_to_message(message):
     for emoji in [f'{i}\N{COMBINING ENCLOSING KEYCAP}' for i in range(10)]:
@@ -68,27 +68,6 @@ def react_to_message(message):
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
-    # Find the target guild by name
-    #target_guild = discord.utils.get(bot.guilds, name=TARGET_GUILD_NAME)
-    #
-    #if target_guild is None:
-    #    print(f'Guild with name "{TARGET_GUILD_NAME}" not found.')
-    #    return
-    # Iterate over all channels in the target guild
-    #for channel in target_guild.text_channels:
-    #    print(f'Loading recent messages from channel: {channel.name}')
-    #    
-    #    # Fetch recent messages from the channel
-    #    async for message in channel.history(limit=1):  # Adjust limit as needed\
-    #            if should_react_to_message(message):
-    #                react_to_message(message)
-#
-    #            # Store reactions if they exist
-    #            for reaction in message.reactions:
-    #                async for user in reaction.users():
-    #                    if should_log_reaction(message, user):
-    #                        await log_reaction(message, reaction, user)
-    
     try:
         synced = await bot.tree.sync()
         print(f'Synced {len(synced)} commands.')
@@ -106,23 +85,9 @@ async def on_reaction_add(reaction, user):
     if should_log_reaction(message, user):
         await log_reaction(message, reaction, user)
 
-#@bot.command(name='get_reactions')
-#async def get_reactions(ctx, message_id: int):
-#    # Fetch the reactions for a given message ID
-#    if message_id in reactions_data:
-#        await ctx.send(f'Reactions for message {message_id}: {reactions_data[message_id]}')
-#    else:
-#        await ctx.send('No reactions recorded for this message ID.')
-
 def format_json(json_string):
     try:
-        # Parse the JSON string into a Python dictionary
-        data = json.loads(json_string)
-        
-        # Convert the dictionary back to a JSON string with indentation
-        formatted_json = json.dumps(data, indent=4)
-        
-        return formatted_json
+        return json.dumps(json.loads(json_string), indent=4)
     except json.JSONDecodeError as e:
         return f"Error decoding JSON: {e}"
 
@@ -136,17 +101,15 @@ async def submit_meme(
     topics: str = None
 ):
     try:
-        # Acknowledge the interaction to prevent timeout
         await interaction.response.defer()
 
-        # Download the attachment
         file_bytes = await visual_file.read()
-        # Prepare the data and files for the request
         data = {
             "TopText": top_text,
             "BottomText": bottom_text,
             "FileName": visual_file.filename,
         }
+
         if(topics is not None):
             try:
                 data["Topics"] = json.loads(topics)
@@ -179,12 +142,9 @@ async def render_meme(
     bottom_text: str = None,
 ):
     try:
-        # Acknowledge the interaction to prevent timeout
         await interaction.response.defer()
 
-        # Download the attachment
         file_bytes = await visual_file.read()
-        # Prepare the data and files for the request
         data = {
             "TopText": top_text,
             "BottomText": bottom_text,
@@ -220,7 +180,6 @@ async def submit_memetext(interaction: discord.Interaction, text: str, position:
     headers = {
         'Content-Type': 'application/json'
     }
-    
     data = {
         'text': text,
         'position': position == "bottomtext",
@@ -234,15 +193,12 @@ async def submit_memetext(interaction: discord.Interaction, text: str, position:
     
     try:
         response = requests.post(API_HOST + "texts", headers=headers, json=data)
-        response.raise_for_status()  # Raise an error for bad status codes
         
-        # Send the result message
         if response.status_code == 201:
             await interaction.followup.send("Memetext created successfully!\n" + "```json\n" + format_json(response.text) + "\n```")
         else:
             await interaction.followup.send("Failed to create meme. Status code: " + str(response.status_code))
     except Exception as e:
-        # Handle exceptions and ensure a response is sent
         await interaction.followup.send(f"An error occurred: {str(e)}")
 
 
@@ -270,15 +226,12 @@ async def delete_element(interaction, id, endpoint):
         if(interaction.user.id != 319532244463255552):
             return await interaction.followup.send("You are not allowed, need more dubloons")
         response = requests.delete(API_HOST + endpoint + id)
-        response.raise_for_status()  # Raise an error for bad status codes
         
-        # Send the result message
         if response.status_code == 204:
             await interaction.followup.send("Element deleted successfully!")
         else:
             await interaction.followup.send("Failed to deleted element. Status code: " + str(response.status_code))
     except Exception as e:
-        # Handle exceptions and ensure a response is sent
         await interaction.followup.send(f"An error occurred: {str(e)}")
 
 @bot.tree.command(name='vote', description='Vote on a votable(Meme, MemeVisual, MemeText)')
@@ -295,23 +248,19 @@ async def vote(interaction: discord.Interaction, elementid: str, votenumber: int
 async def post_vote(followup, headers, data, should_post_success):
     try:
         response = requests.post(API_HOST + "votes", headers=headers, data=data)
-        response.raise_for_status()  # Raise an error for bad status codes
     
-        # Send the result message
         if response.status_code == 201 or response.status_code == 200:
             if should_post_success:
                 await followup.send("Vote successful!\n" + "```json\n" + format_json(response.text) + "\n```")
         else:
             await followup.send("Failed to vote. Status code: " + str(response.status_code))
     except Exception as e:
-        # Handle exceptions and ensure a response is sent
         await followup.send(f"An error occurred: {str(e)}")
 
 def prepare_vote_data(user, elementid, votenumber):
     headers = {
         'Bot_Secret': BOT_SECRET
     }
-
     data = {
         'ElementIDs': [elementid],
         'VoteNumber': votenumber,
@@ -330,15 +279,12 @@ async def submit_memevisual(
     topics: str = None
 ):
     try:
-        # Acknowledge the interaction to prevent timeout
         await interaction.response.defer()
 
-        # Download the attachment
         file_bytes = await file.read()
-        
-        # Prepare the data and files for the request
         data = {
         }
+
         if(topics is not None):
             try:
                 data["Topics"] = json.loads(topics)
@@ -349,17 +295,14 @@ async def submit_memevisual(
             "File": (file.filename, file_bytes)
         }
         
-        # Make the request to the API
         response = requests.post(API_HOST + "Visuals", data=data, files=files)
         
-        # Send the result message
         if response.status_code == 201:
             await interaction.followup.send("MemeVisual created successfully!\n" + "```json\n" + format_json(response.text) + "\n```")
         else:
             await interaction.followup.send("Failed to create MemeVisual. Status code: " + str(response.status_code))
 
     except Exception as e:
-        # Handle exceptions and ensure a response is sent
         await interaction.followup.send(f"An error occurred: {str(e)}")
 
 bot.run(TOKEN)
